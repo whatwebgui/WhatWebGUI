5b98a874dee68562e2b9be5142808b76e92f48ef
anderra57 <49227774+anderra57@users.noreply.github.com>
2020-11-14 23:16:22 +0100

    database controller updated

--- src/main/java/ehu/isad/controllers/db/DBController.java
+++ src/main/java/ehu/isad/controllers/db/DBController.java
@@ -1,4 +1,44 @@
 package ehu.isad.controllers.db;
 
+import java.sql.*;
+
 public class DBController {
+
+    Connection conn=null;
+    private static DBController controller = new DBController();
+
+    private DBController() {
+        this.conOpen();
+    }
+
+    public static DBController getController() {
+        return controller;
+    }
+
+    private void conOpen() {
+
+        try {
+            conn = DriverManager.getConnection("jdbc:sqlite:whatweb.db.sql");
+            System.out.println("Connection to SQLite has been established.");
+        } catch (SQLException ex) {
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
+        }
+
+    }
+
+    private void conClose() {
+
+        if (conn != null)
+            try {
+                conn.close();
+                System.out.println("Connection to SQLite has been terminated.");
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+    }
+
+
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,5 +5,7 @@ open module whatwebgui {
   requires javafx.graphics;
   requires javafx.controls;
   requires javafx.fxml;
+    requires java.desktop;
+  requires java.sql;
   exports ehu.isad;
 }
