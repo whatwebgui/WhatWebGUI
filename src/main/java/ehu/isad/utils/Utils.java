522977d41844be5edda461b79e4b1d59a1d136de
Kerman-Sanjuan <kermansanjuanmalax@gmail.com>
2020-11-16 12:01:59 +0100

    Added properties support

---
+++ src/main/java/ehu/isad/utils/Utils.java
@@ -0,0 +1,23 @@
+package ehu.isad.utils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class Utils {
+
+    public static Properties getProperties()  {
+        Properties properties = null;
+
+        try (InputStream in = Utils.class.getResourceAsStream("/setup.properties")) {
+            properties = new Properties();
+            properties.load(in);
+
+        } catch (
+                IOException e) {
+            e.printStackTrace();
+        }
+
+        return properties;
+    }
+}
