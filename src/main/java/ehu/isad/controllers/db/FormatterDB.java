ce9e3e90b6d2e976aea2a9dfbd753bf81a31da46
anderra57 <49227774+anderra57@users.noreply.github.com>
2020-11-16 14:59:33 +0100

    addFormatToDB created

--- src/main/java/ehu/isad/controllers/db/FormatterDB.java
+++ src/main/java/ehu/isad/controllers/db/FormatterDB.java
@@ -8,20 +8,23 @@ public class FormatterDB {
     private static DBController dbcontroller = DBController.getController();
     private FormatterDB() {}
 
-    private Boolean exists(String url,String format) throws SQLException {
+    private Boolean exists(String domain,String format) throws SQLException {
+        // does {domain}.{format} exist?
         String q1 = "Select * from cache  where url = ";
         String q2 = " AND ";
         String q3 = " = TRUE;";
         StringBuilder query = new StringBuilder();
-        query.append(q1).append(url).append(q2).append(format).append(q3);
-        System.out.println( query.append(q1).append(url).append(q2).append(format).append(q3));
+        query.append(q1).append(domain).append(q2).append(format).append(q3);
+        System.out.println( query.append(q1).append(domain).append(q2).append(format).append(q3));
         return dbcontroller.execSQL(query.toString()).next();
     }
 
-    private void addDB(String url, String format) {
+    private void addFormatToDB(String domain, String format) {
         //This method adds the "check" to the database.
-        String q = "Insert into cache(domain,"+format+") values("+url+", TRUE)";
-        dbcontroller.execSQL(q);
+        String q1 = "INSERT OR IGNORE (domain) VALUES ('"+domain+"')";
+        String q2 = "UPDATE cache SET "+format+" = TRUE WHERE domain='"+domain+"';";
+        dbcontroller.execSQL(q1);
+        dbcontroller.execSQL(q2);
     }
 
 
