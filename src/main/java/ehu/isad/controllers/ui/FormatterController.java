cb3bc07e11dbd5c18763831a8a3f786863daa3d5
adeiarias <adeiariasupv@gmail.com>
2020-11-17 07:04:35 +0100

    onScanButton thread, commandThread done, but bufferReader thread not working properly

--- src/main/java/ehu/isad/controllers/ui/FormatterController.java
+++ src/main/java/ehu/isad/controllers/ui/FormatterController.java
@@ -3,6 +3,7 @@ package ehu.isad.controllers.ui;
 import ehu.isad.controllers.db.FormatterDB;
 import ehu.isad.model.Extension;
 import ehu.isad.utils.Utils;
+import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -17,6 +18,7 @@ import javafx.scene.layout.Pane;
 import java.awt.*;
 import java.io.*;
 import java.net.URL;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
@@ -46,6 +48,8 @@ public class FormatterController {
     @FXML
     private Button btn_show;
 
+    private Thread thread;
+
     FormatterDB formatterDB = FormatterDB.getController();
     private String path= Utils.getProperties().getProperty("pathToCacheFolder");
 
@@ -54,8 +58,23 @@ public class FormatterController {
         Button btn = (Button) event.getSource();
         if (btn_scan.equals(btn)) {
             String newLine = System.getProperty("line.separator");
-            textArea.setText(getOutput().stream().collect(Collectors.joining(newLine)));
-            textArea.setWrapText(true);
+
+            thread = new Thread( () -> {
+                String result = null;
+                try {
+                    result = getOutput().stream().collect(Collectors.joining(newLine));
+                } catch (IOException ioException) {
+                    ioException.printStackTrace();
+                }
+                String finalResult = result;
+                Platform.runLater( () -> {
+                    textArea.setText(finalResult);
+                    textArea.setWrapText(true);
+                } );
+
+            });
+            thread.start();
+
         }
         else if (btn_clear.equals(btn)) {
             textArea.clear();
@@ -69,48 +88,86 @@ public class FormatterController {
 
     private List<String> getOutput() throws IOException {
         List<String> emaitza = new LinkedList<>();
+        String domain = textField.getText().replace("/", "").split(":")[1];
         try {
-            String domain = textField.getText().replace("/","").split(":")[1];
-            formatterDB.addDomainToDB(domain);
-            if (!FormatterDB.getController().formatExists(domain,combo.getValue().getType())){
-                executeCommand(combo.getValue(),domain); //This will execute and create the file.
-                formatterDB.addFormatToDB(domain,combo.getValue().getType());
-            }
-            Thread.sleep(10000);
-            //This loads the file with the domain name.
+            Thread commandThread = new Thread( () -> {
+                formatterDB.addDomainToDB(domain);
+                try {
+                    if (!FormatterDB.getController().formatExists(domain, combo.getValue().getType())) {
+                        executeCommand(combo.getValue(), domain); //This will execute and create the file.
+                        formatterDB.addFormatToDB(domain, combo.getValue().getType());
+                    }
+                } catch (SQLException throwables) {
+                    throwables.printStackTrace();
+                } catch (IOException ioException) {
+                    ioException.printStackTrace();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            });
+            commandThread.start();
 
-            BufferedReader input = new BufferedReader(new FileReader(path+domain+combo.getValue().getExtension()));
-            String line;
-            while ((line = input.readLine()) != null) { emaitza.add(line); }
-            input.close();
+            Thread bufferReaderThread = new Thread( () -> {
+                //This loads the file with the domain name.
+                BufferedReader input = null;
+                try {
+                    input = new BufferedReader(new FileReader(path + domain + combo.getValue().getExtension()));
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+                String line=null;
+                while (true) {
+                    try {
+                        if (!((line = input.readLine()) != null)) break;
+                    } catch (IOException ioException) {
+                        ioException.printStackTrace();
+                    }
+                    emaitza.add(line);
+                }
+                try {
+                    input.close();
+                } catch (IOException ioException) {
+                    ioException.printStackTrace();
+                }
+            });
+            bufferReaderThread.start();
+
         } catch (Exception err) {
             err.printStackTrace();
         }
         return emaitza;
     }
 
-    private void executeCommand(Extension ext, String domain) throws IOException {
+    private void executeCommand(Extension ext, String domain) throws IOException, InterruptedException {
+            String target = textField.getText();
+            String type = ext.getType();
+            String extension = ext.getExtension();
+            String command = null;
+            switch (type) {
+                case "shell":
+                    command = "whatweb --color=never --log-brief=" + path + domain + extension + " " + target;
 
-        String target = textField.getText();
-        String type = ext.getType();
-        String extension = ext.getExtension();
-        String command = null;
-        switch(type) {
-            case "shell":
-                command = "whatweb --color=never --log-brief=" +path+domain+extension+" "+target;
-                break;
-            case "ruby":
-                command = "whatweb --color=never --log-object=" +path+domain+extension+" "+target;
-                break;
-            default:
-                command = "whatweb --color=never --log-"+type+"=" +path+domain+extension+" "+target;
-                break;
-        }
-        if (System.getProperty("os.name").toLowerCase().contains("win")) {
-            Runtime.getRuntime().exec(System.getenv("wsl " + command));
-        } else {
-            Runtime.getRuntime().exec(command);
-        }
+                    break;
+                case "ruby":
+                    command = "whatweb --color=never --log-object=" + path + domain + extension + " " + target;
+                    break;
+                default:
+                    command = "whatweb --color=never --log-" + type + "=" + path + domain + extension + " " + target;
+                    break;
+            }
+            if (System.getProperty("os.name").toLowerCase().contains("win")) {
+                try {
+                    Runtime.getRuntime().exec(System.getenv("wsl " + command));
+                } catch (IOException ioException) {
+                    ioException.printStackTrace();
+                }
+            } else {
+                try {
+                    Runtime.getRuntime().exec(command);
+                } catch (IOException ioException) {
+                    ioException.printStackTrace();
+                }
+            }
     }
 
     @FXML
