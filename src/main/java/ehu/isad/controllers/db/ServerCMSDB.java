f0581c7d467abe71d9dfd8d48037fce2dc817f82
anderra57 <49227774+anderra57@users.noreply.github.com>
2020-11-21 19:45:32 +0100

    updated queries, now they support every cms & server (via list from a .txt file)

--- src/main/java/ehu/isad/controllers/db/ServerCMSDB.java
+++ src/main/java/ehu/isad/controllers/db/ServerCMSDB.java
@@ -1,11 +1,14 @@
 package ehu.isad.controllers.db;
 
 import ehu.isad.model.ServerCMSModel;
+import ehu.isad.utils.Utils;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
+import java.io.*;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class ServerCMSDB {
 
@@ -38,8 +41,10 @@ public class ServerCMSDB {
     }
 
     public ObservableList<ServerCMSModel> getCMSDB(){
+        String list = openFile("cms");
         ObservableList<ServerCMSModel> results = FXCollections.observableArrayList();
-        String query = "SELECT DISTINCT q.target,q.name,q.version,q.date FROM ( SELECT t.target, CASE WHEN p.name in ('WordPress','Joomla','phpMyAdmin','Drupal') THEN p.name ELSE 'unknown' END AS name, CASE WHEN p.name in ('WordPress','Joomla','phpMyAdmin','Drupal') THEN s.version WHEN s.version = '0' THEN 'unknown' ELSE 'unknown' END AS version, s.date FROM ((targets t natural join scans s) natural join plugins p) join servercmsDate s ON t.target_id=s.id WHERE t.status=200 and p.name in ('WordPress','Joomla','phpMyAdmin','Drupal') UNION SELECT t.target, CASE WHEN p.name = 'WordPress' THEN p.name ELSE 'unknown' END AS name, CASE WHEN p.name = 'Wordpress' THEN s.version WHEN s.version = '0' THEN 'unknown' ELSE 'unknown' END AS version, s.date FROM ((targets t natural join scans s) natural join plugins p) join servercmsDate s ON t.target_id=s.id WHERE t.status=200 and p.name not in ('WordPress','Joomla','phpMyAdmin','Drupal') GROUP BY t.target ORDER BY s.date DESC ) q GROUP BY q.target ORDER by q.date DESC";
+        //String query = "SELECT DISTINCT q.target,q.name,q.version,q.date FROM ( SELECT t.target, CASE WHEN p.name in ('WordPress','Joomla','phpMyAdmin','Drupal') THEN p.name ELSE 'unknown' END AS name, CASE WHEN p.name in ('WordPress','Joomla','phpMyAdmin','Drupal') THEN s.version WHEN s.version = '0' THEN 'unknown' ELSE 'unknown' END AS version, s.date FROM ((targets t natural join scans s) natural join plugins p) join servercmsDate s ON t.target_id=s.id WHERE t.status=200 and p.name in ('WordPress','Joomla','phpMyAdmin','Drupal') UNION SELECT t.target, CASE WHEN p.name = 'WordPress' THEN p.name ELSE 'unknown' END AS name, CASE WHEN p.name = 'Wordpress' THEN s.version WHEN s.version = '0' THEN 'unknown' ELSE 'unknown' END AS version, s.date FROM ((targets t natural join scans s) natural join plugins p) join servercmsDate s ON t.target_id=s.id WHERE t.status=200 and p.name not in ('WordPress','Joomla','phpMyAdmin','Drupal') GROUP BY t.target ORDER BY s.date DESC ) q GROUP BY q.target ORDER by q.date DESC";
+        String query = "SELECT DISTINCT q.target,q.name,q.version,q.date FROM ( SELECT t.target, CASE WHEN p.name in " + list + " THEN p.name ELSE 'unknown' END AS name, CASE WHEN p.name in " + list + " AND s.version not in ('0','') THEN s.version WHEN s.version in ('0','') THEN 'unknown' ELSE 'unknown' END AS version, s.date FROM ((targets t natural join scans s) natural join plugins p) join servercmsDate s ON t.target_id=s.id WHERE t.status=200 and p.name in " + list + "UNION SELECT t.target, 'unknown' AS name,'unknown' AS version, s.date FROM ((targets t natural join scans s) natural join plugins p) join servercmsDate s ON t.target_id=s.id WHERE t.status=200 GROUP BY t.target ORDER BY s.date DESC ) q GROUP BY q.target ORDER by q.date DESC";
         ResultSet rs = dbcontroller.execSQL(query);
         String url;
         String plugin;
@@ -65,7 +70,10 @@ public class ServerCMSDB {
 
     public ObservableList<ServerCMSModel> getServerDB(){
         ObservableList<ServerCMSModel> results = FXCollections.observableArrayList();
-        String query = "SELECT DISTINCT q.target,q.name,q.version,q.date FROM ( SELECT t.target, CASE WHEN p.name = 'Apache' THEN p.name ELSE 'unknown' END AS name, CASE WHEN p.name = 'Apache' THEN s.version WHEN s.version = '0' THEN 'unknown' ELSE 'unknown' END AS version, s.date FROM ((targets t natural join scans s) natural join plugins p) join servercmsDate s ON t.target_id=s.id WHERE t.status=200 and p.name='Apache' UNION SELECT t.target, CASE WHEN p.name = 'Apache' THEN p.name ELSE 'unknown' END AS name, CASE WHEN p.name = 'Apache' THEN s.version WHEN s.version = '0' THEN 'unknown' ELSE 'unknown' END AS version, s.date FROM ((targets t natural join scans s) natural join plugins p) join servercmsDate s ON t.target_id=s.id WHERE t.status=200 and p.name!='Apache' GROUP BY t.target ORDER BY s.date DESC ) q GROUP BY q.target ORDER by q.date DESC";
+        //String query = "SELECT DISTINCT q.target,q.name,q.version,q.date FROM ( SELECT t.target, CASE WHEN p.name = 'Apache' THEN p.name ELSE 'unknown' END AS name, CASE WHEN p.name = 'Apache' THEN s.version WHEN s.version = '0' THEN 'unknown' ELSE 'unknown' END AS version, s.date FROM ((targets t natural join scans s) natural join plugins p) join servercmsDate s ON t.target_id=s.id WHERE t.status=200 and p.name='Apache' UNION SELECT t.target, CASE WHEN p.name = 'Apache' THEN p.name ELSE 'unknown' END AS name, CASE WHEN p.name = 'Apache' THEN s.version WHEN s.version = '0' THEN 'unknown' ELSE 'unknown' END AS version, s.date FROM ((targets t natural join scans s) natural join plugins p) join servercmsDate s ON t.target_id=s.id WHERE t.status=200 and p.name!='Apache' GROUP BY t.target ORDER BY s.date DESC ) q GROUP BY q.target ORDER by q.date DESC";
+        String list = openFile("server");
+        String query = "SELECT DISTINCT q.target,q.name,q.version,q.date FROM ( SELECT t.target, CASE WHEN p.name in " + list + " THEN p.name ELSE 'unknown' END AS name, CASE WHEN p.name in " + list + " AND s.version not in ('0','') THEN s.version WHEN s.version in ('0','') THEN 'unknown' ELSE 'unknown' END AS version, s.date FROM ((targets t natural join scans s) natural join plugins p) join servercmsDate s ON t.target_id=s.id WHERE t.status=200 and p.name in " + list + "UNION SELECT t.target, 'unknown' AS name,'unknown' AS version, s.date FROM ((targets t natural join scans s) natural join plugins p) join servercmsDate s ON t.target_id=s.id WHERE t.status=200 GROUP BY t.target ORDER BY s.date DESC ) q GROUP BY q.target ORDER by q.date DESC";
+        System.out.println(query);
         ResultSet rs = dbcontroller.execSQL(query);
         String url;
         String plugin;
@@ -83,6 +91,28 @@ public class ServerCMSDB {
             e.printStackTrace();
         }
         return  results;
+    }
+
+    private String openFile(String tab) {
+        StringBuffer sb=new StringBuffer();
+        FileReader fr;
+        try {
+            if (tab.equals("cms")) {
+                fr = new FileReader(new File(ServerCMSDB.class.getResource("/cmslist.txt").getFile()));
+                sb.append("('WordPress', 'Joomla', 'Drupal', 'phpMyAdmin',");
+            } else {
+                fr=new FileReader(new File(ServerCMSDB.class.getResource("/serverlist.txt").getFile()));
+                sb.append("('Apache', 'nginx', ");
+            }
+            BufferedReader br=new BufferedReader(fr);
+            String line;
+            while((line=br.readLine())!=null) {
+                sb.append("'").append(line).append("', ");
+            }
+            sb.append("'EndOfPluginList')");
+            fr.close();
+        } catch(IOException e){ e.printStackTrace();}
+        return sb.toString();
     }
 
     public void addDate(String targ){
